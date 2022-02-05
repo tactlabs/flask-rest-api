7e04d731d1eb96556540473ae4d20a2ae3aed4df
prakash <prashprakash1211@gmail.com>
2022-02-05 21:34:47 +0530

    added docker-compose file

--- app.py
+++ app.py
@@ -30,41 +30,6 @@ def start():
 
     return result_dict
 
-'''
-    Possible urls:
-    http://0.0.0.0:5003/get/user/info
-'''
-@app.route('/get/user/info')
-def get_user_info():
-
-    result_dict = {
-        'name' : 'Kevin',
-        'age' : '18'
-    }
-
-    return result_dict
-
-'''
-    Possible urls:
-    http://0.0.0.0:5003/multiply?number=2
-
-'''
-@app.route('/multiply')
-def do_multiply():
-
-    # It should multiply by 6
-
-    my_number = int(request.args['number'])
-
-    result = 6 * my_number
-
-    result_dict = {
-        'result' : result,
-        'my_number' : my_number
-    }
-
-    return result_dict
-
 if __name__== "__main__":
     
     app.run(
